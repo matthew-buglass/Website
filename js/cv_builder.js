a6c83f0a324c3d218cfd972b4805627311886081
matthew-buglass <buglassmatthew@gmail.com>
2023-12-12 12:17:29 -0600

    abstracted out the build of cv items from data files

--- js/cv_builder.js
+++ js/cv_builder.js
@@ -4,7 +4,7 @@ function get_cv_html() {
     return section_elem.getElementsByClassName("dummy")[0];
 }
 
-function get_work_data() {
+function get_json_data(path) {
     return new Promise((resolve, reject) => {
         /* Get a the section element */
         xhttp = new XMLHttpRequest();
@@ -14,9 +14,20 @@ function get_work_data() {
                 if (this.status == 404) { reject("Data not found") }
             }
         }
-        xhttp.open("GET", "/data/work.json", true);
+        xhttp.open("GET", path, true);
         xhttp.send();
     })
+}
+
+function append_elements(super_elem, sub_elem_template, sub_elem_data) {
+    sub_elem_data.forEach(elem => {
+        let new_sec = sub_elem_template.cloneNode(deep=true);
+        new_sec.getElementsByClassName('title')[0].innerHTML = elem.title
+        new_sec.getElementsByClassName('location')[0].innerHTML = elem.location;
+        new_sec.getElementsByClassName('dates')[0].innerHTML = elem.dates;
+        new_sec.getElementsByClassName('details')[0].innerHTML = elem.details;
+        super_elem.innerHTML += new_sec.innerHTML;
+    });
 }
 
 async function buildCV() {
@@ -24,24 +35,49 @@ async function buildCV() {
     var section_elem = get_cv_html();
 
     /* Build work data */
-    get_work_data().then(
+    get_json_data("/data/work.json").then(
         function(resp) {
             let work_section = document.getElementById("work");
-            console.log(work_section);
-            resp.forEach(elem => {
-                let new_sec = section_elem.cloneNode(deep=true);
-                new_sec.getElementsByClassName('title')[0].innerHTML = elem.title
-                new_sec.getElementsByClassName('location')[0].innerHTML = elem.location;
-                new_sec.getElementsByClassName('dates')[0].innerHTML = elem.dates;
-                new_sec.getElementsByClassName('details')[0].innerHTML = elem.details;
-                console.log(new_sec)
-                work_section.innerHTML += new_sec.innerHTML;
-                
-            });
-            console.log(work_section);
+            append_elements(work_section, section_elem, resp);
         },
         function(err) {
             let work_section = document.getElementById("work");
+            work_section.innerHTML = err;
+        }
+    );
+
+    /* Build volunteer data */
+    get_json_data("/data/volunteer.json").then(
+        function(resp) {
+            let work_section = document.getElementById("volunteer");
+            append_elements(work_section, section_elem, resp);
+        },
+        function(err) {
+            let work_section = document.getElementById("volunteer");
+            work_section.innerHTML = err;
+        }
+    );
+
+    /* Build academic data */
+    get_json_data("/data/academic.json").then(
+        function(resp) {
+            let work_section = document.getElementById("academic");
+            append_elements(work_section, section_elem, resp);
+        },
+        function(err) {
+            let work_section = document.getElementById("academic");
+            work_section.innerHTML = err;
+        }
+    );
+
+    /* Build awards data */
+    get_json_data("/data/awards.json").then(
+        function(resp) {
+            let work_section = document.getElementById("awards");
+            append_elements(work_section, section_elem, resp);
+        },
+        function(err) {
+            let work_section = document.getElementById("awards");
             work_section.innerHTML = err;
         }
     );
